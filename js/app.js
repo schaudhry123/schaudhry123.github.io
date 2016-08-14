79e84fc54eaec76c1866509e8166fbde2b723c1a
Samir Chaudhry <schaudhry123@gmail.com>
2016-08-13 21:55:10 -0700

    Completely updated design

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-$.material.init()
+// Header function to add/remove sticky class when scrolling
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+        $('.page-header').addClass("sticky");
+    }
+    else {
+        $('.page-header').removeClass("sticky");
+    }
+});
+
+// Smooth scrolling function for header links
+function scrollToAnchor(id) {
+    var tag = $("#" + id);
+    $('html,body').animate({ scrollTop: tag.offset().top - 65}, 'slow');
+}
+
+// Enable smooth scrolling for links
+$("#about-link").click(function() {
+    scrollToAnchor('about')
+});
+$("#projects-link").click(function() {
+    scrollToAnchor('projects')
+});
+$("#contact-link").click(function() {
+    scrollToAnchor('contact')
+});
